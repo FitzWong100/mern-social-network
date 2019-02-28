1d5c2b1f7c8c8b22f534a13017c7c0c7bd11e4ee
Fitzgerald Wong <FitzWong100@users.noreply.github.com>
2019-02-28 17:31:06 -0500

    mern-social-network

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://fitz:kittytravel1@mern-social-network-fitz-wong-shard-00-00-r4ip9.mongodb.net:27017,mern-social-network-fitz-wong-shard-00-01-r4ip9.mongodb.net:27017,mern-social-network-fitz-wong-shard-00-02-r4ip9.mongodb.net:27017/test?ssl=true&replicaSet=mern-social-network-fitz-wong-shard-0&authSource=admin&retryWrites=true"
+};
